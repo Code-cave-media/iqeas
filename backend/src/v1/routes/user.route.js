22ae5e4ae0aed77228e4013c6feacdd1a772bee9
Aromal <aromals161@gmail.com>
2025-07-09 14:49:15 +0530

    added a new route to betch both users and team data also schema

--- backend/src/v1/routes/user.route.js
+++ backend/src/v1/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   createNewUser,
+  getUsersController,
   toggleUserStatus,
 } from "../controllers/user.controller.js";
 
@@ -9,5 +10,7 @@ const router = express.Router();
 router.post("/admin/create/create-user", createNewUser);
 
 router.patch("/admin/user/:id/status", toggleUserStatus);
+
+router.get("/admin/get-users", getUsersController);
 
 export default router;
