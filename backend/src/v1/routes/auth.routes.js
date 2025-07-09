c84a20be9a170466aa88c59162729ad2366a960a
Muhammed Irshad <muhammedirshadismail0722@gmail.com>
2025-07-09 12:41:49 +0530

    Save local changes

--- backend/src/v1/routes/auth.routes.js
+++ backend/src/v1/routes/auth.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { login } from "../controllers/auth.controller.js";
+import { getCurrentUser, login } from "../controllers/auth.controller.js";
 import { authenticateToken } from "../middleware/authMiddleware.js";
-import { allowRoles } from "../utils/verification.js";
 
 const router = express.Router();
 
 router.post("/auth/login", login);
+router.get("/auth/me", authenticateToken, getCurrentUser);
 
 export default router;
--- backend/src/v1/services/user.service.js
+++ backend/src/v1/services/user.service.js
@@ -3,16 +3,16 @@ import bcrypt from "bcryptjs";
 import { generatePassword } from "../utils/passwordGenerator.js";
 import { uuidGenerator } from "../utils/uuidGenerator.js";
 
-export async function createUser(email, phonenumber, name, role) {
-  const password = generatePassword(email, phonenumber);
+export async function createUser(email, phoneNumber, name, role, active = true) {
+  const password = generatePassword(email, phoneNumber);
   const hashedPassword = await bcrypt.hash(password, 10);
   const uniqueId = uuidGenerator();
 
   const userResult = await pool.query(
-    `INSERT INTO users (email, phonenumber, name, role, password, active, user_id) 
+    `INSERT INTO users (email, phoneNumber, name, role, password, active, user_id) 
      VALUES ($1, $2, $3, $4, $5, $6, $7) 
-     RETURNING id, email, phonenumber, name, role, active`,
-    [ email, phonenumber, name, role, hashedPassword, true, uniqueId]
+     RETURNING id, email, phoneNumber, name, role, active`,
+    [ email, phoneNumber, name, role, hashedPassword, active, uniqueId]
   );
 
   return {
