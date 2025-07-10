1815a66a980dffb5ab41984f5f2a443015ffa1e2
Aromal <aromals161@gmail.com>
2025-07-10 12:40:34 +0530

    added leader_id in teams and is_deleated in users

--- backend/src/v1/routes/user.route.js
+++ backend/src/v1/routes/user.route.js
@@ -5,12 +5,16 @@ import {
   toggleUserStatus,
 } from "../controllers/user.controller.js";
 
+
+import { authenticateToken } from "../middleware/authMiddleware.js"
+import { allowRoles } from "../utils/verification.js";
+
 const router = express.Router();
 
 router.post("/admin/create/create-user", createNewUser);
 
 router.patch("/admin/user/:id/status", toggleUserStatus);
 
-router.get("/admin/get-users", getUsersController);
+router.get("/admin/get-users",authenticateToken, allowRoles("admin"), getUsersController);
 
 export default router;
