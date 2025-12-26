261a526c99b82a55db2b32a390ea764c411b4e24
DeveloperAromal <developeraromal@gmail.com>
2025-12-26 17:23:20 +0530

    updated backend config

--- frontend/src/config/backend.ts
+++ frontend/src/config/backend.ts
@@ -2,7 +2,7 @@
 import { get } from "http";
 
 // export const API_URL = `${import.meta.env.VITE_BACKEND_URL}/api/v1`;
-export const API_URL = `https://43246a342bab.ngrok-free.app/api/v1`;
+export const API_URL = `https://iqeas-x6eg.onrender.com/api/v1`;
 
 export const API_ENDPOINT = {
   VERIFY_USER: `${API_URL}/auth/me `,
